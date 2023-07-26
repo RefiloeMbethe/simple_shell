d94c99347e62a964abb4037659c788d4974bfa04
RefiloeMbethe <129064322+RefiloeMbethe@users.noreply.github.com>
2023-07-26 17:10:21 +0200

    Update memory_freed.c

--- memory_freed.c
+++ memory_freed.c
@@ -1,50 +1,20 @@
 #include "main.h"
 
 /**
- * free_memry_exit - free all the memory allocated and leave
- * @cmds: pointer to command memory of strings.
+ * free_and_exit - Free all the memory allocated for command and exit the program.
+ * @cmds: Pointer to allocated memory.
  * Return: Nothing.
  */
-
-void free_memry_exit(char **cmds)
+void free_and_exit(char **cmds)
 {
-	size_t i = 0;
+    if (cmds == NULL)
+        return;
 
-	if (cmds == NULL)
-		return;
+    for (size_t i = 0; cmds[i]; i++)
+    {
+        free(cmds[i]);
+    }
 
-	while (cmds[i])
-	{
-		free(cmds[i]);
-		i++;
-	}
-
-	if (cmds[i] == NULL)
-		free(cmds[i]);
-	free(cmds);
-	exit(0);
-}
-
-
-/**
- * memry_freed - frees all the memory allocated for command
- * @cmds: Pointer to allocated memory
- * Return: nothing.
- */
-void memry_freed(char **cmds)
-{
-	size_t i = 0;
-
-	if (cmds == NULL)
-		return;
-
-	while (cmds[i])
-	{
-		free(cmds[i]);
-		i++;
-	}
-
-	if (cmds[i] == NULL)
-		free(cmds[i]);
-	free(cmds);
+    free(cmds);
+    exit(0);
 }
