e63b9f6c52dd55f085b07552536d92600fb2e8ed
RefiloeMbethe <129064322+RefiloeMbethe@users.noreply.github.com>
2023-07-27 15:31:39 +0200

    Update main.h

--- main.h
+++ main.h
@@ -12,19 +12,19 @@
 /* Function Prototypes */
 
 /* Utility functions */
-char **path_direc(char **env);
+char *get_env_value(char **env, const char *var_name);
 int strings_compare(const char *string1, const char *string2);
 unsigned int strings_length(const char *string);
-char *strings_copy(char *desti, const char *sources);
-char *string_conc(char *desti, const char *sources);
+char *string_copy(char *destination, const char *source);
+char *string_concat(char *destination, const char *source);
 int string_to_int(const char *string);
 
 /* Shell functionality */
-void exe_command(char **commands, char *names, char **env, int cycle);
-void error_messages(const char *names, int cycle, char **commands);
-void prints_environ(char **env);
+void execute_command(char **commands, char *shell_name, char **env, int cycle);
+void error_message(char *shell_name, int cycle, char *command);
+void print_environment(char **env);
 int changes_directories(const char *paths);
-void child_process(char **cmds, char *names, char **env, int cycle);
+void child_process(char **cmds, char *shell_name, char **env, int cycle);
 char **split_string(char *buffers, const char *delimiter);
 void prompt(void);
 void signal_handler(int prompt_handle);
@@ -32,7 +32,7 @@ void exits_shell(char **commands);
 void handles_EOF(char *buff_strings);
 
 /* Memory management */
-void free_memry_exit(char **cmds);
+void free_and_exit(char **cmds);
 void memry_freed(char **cmds);
 
 /* Main function */
